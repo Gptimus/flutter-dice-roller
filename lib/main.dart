af8c927d9e7afb38c5637271c3474d775ab34782
Guerth Manzala <guerthmanzala@gmail.com>
2023-07-04 10:36:47 +0100

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:basics/gradient_container.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(colors: [
+          Color.fromARGB(255, 26, 2, 80),
+          Color.fromARGB(255, 55, 10, 114),
+        ]),
+      ),
+    ),
+  );
+}
